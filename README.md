8bac7e0c257786a6ebfd8c133ebe2892efa19b2c
Silent Cat <ngocphuocnguyen312@gmail.com>
2024-12-20 19:05:59 +0700

    add doc

--- README.md
+++ README.md
@@ -13,13 +13,72 @@ and the Flutter guide for
 
 [![codecov](https://codecov.io/github/SilentCatD/accessible_text_span/graph/badge.svg?token=wTay7Q3ild)](https://codecov.io/github/SilentCatD/accessible_text_span)
 
+[![pub package](https://img.shields.io/pub/v/accessible_text_span?color=green&include_prereleases&style=plastic)](https://pub.dev/packages/accessible_text_span)
+
+<img src="https://github.com/SilentCatD/accessible_text_span/blob/main/assets/focusable.gif?raw=true" width="200px">
 
 ## Features
 
+First import the widget
 
-## Getting started
+```dart
+import 'package:accessible_text_span/accessible_text_span.dart';
+```
+
+When implementing accessibility, enabling keyboard navigation for interactive elements is crucial.
+
+However, due to a current
+limitation ([Flutter issue #138692](https://github.com/flutter/flutter/issues/138692)),
+navigating and interacting with a `TextSpan` is not yet supported.
+
+This package is designed to address this limitation by providing a solution to make `TextSpan`
+accessible for both TalkBack and keyboard navigation when the user pressing **TAB** or **ENTER**
 
 ## Usage
 
-## Additional information
+```dart
+AccessibleRichText(
+  TextSpan(
+    children: [
+      TextSpan(
+        text: "link 1",
+        recognizer: TapGestureRecognizer()
+          ..onTap = () {
+            // on link tap or activated by keyboard
+          },
+      ),
+      const TextSpan(
+        text: "and ",
+      ),
+      TextSpan(
+        text: "link 2",
+        recognizer: TapGestureRecognizer()
+          ..onTap = () {
+            // on link tap or activated by keyboard
+          },
+      ),
+    ],
+  ),
+  focusedStyle: (context) {
+    return DefaultTextStyle.of(context).style.copyWith(
+          decoration: TextDecoration.underline,
+          backgroundColor: Colors.grey,
+          color: Colors.purple,
+        );
+  },
+),
+```
 
+Replace the `Text.rich` or `RichText` widget in your app with the `AccessibleRichText` widget.
+
+To make a specific `TextSpan` focusable, it must include a `TextSpan.recognizer` of type 
+`TapGestureRecognizer`.
+
+The visual representation of a focused `TextSpan` can be customized using the 
+`AccessibleRichText.focusedStyle` property.
+
+For manual manipulation and management of `FocusNode`, you can create and provide your own instance 
+of `FocusableTextSpanBuilder`.
+
+Pressing **TAB** will navigating through created focusable `TextSpan` while **ENTER** will activate
+the associated `TapGestureRecognizer.onTap` function.
--- lib/src/accessible_rich_text.dart
+++ lib/src/accessible_rich_text.dart
@@ -1,16 +1,31 @@
 import 'dart:ui' as ui;
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'focusable_text_span_builder.dart';
 import 'type.dart';
 
+/// Default focused style for interactive [TextSpan] when focused by keyboard
 TextStyle defaultFocusedTextStyle(BuildContext context) {
   return DefaultTextStyle.of(context)
       .style
       .copyWith(backgroundColor: Theme.of(context).focusColor);
 }
 
+/// Alternative of [RichText] or [Text.rich] widget that allow [TextSpan] with
+/// [TapGestureRecognizer] to be focused and activated using keyboard
+/// Support accessibility use cases for keyboard navigation user following
+/// https://github.com/flutter/flutter/issues/138692
+/// Talkback is also supported when the interactive [TextSpan] is focused
 class AccessibleRichText extends StatefulWidget {
+  /// Creates a text widget with [TextSpan]s that are focusable when
+  /// [TextSpan.recognizer] is [TapGestureRecognizer].
+  ///
+  /// The following subclasses of [InlineSpan] may be used to build rich text:
+  ///
+  /// * [TextSpan]s define text and children [InlineSpan]s.
+  /// * [WidgetSpan]s define embedded inline widgets.
   const AccessibleRichText(
     this.textSpan, {
     super.key,
@@ -21,12 +36,6 @@ class AccessibleRichText extends StatefulWidget {
     this.locale,
     this.softWrap,
     this.overflow,
-    @Deprecated(
-      'Use textScaler instead. '
-      'Use of textScaleFactor was deprecated in preparation for the upcoming nonlinear text scaling support. '
-      'This feature was deprecated after v3.12.0-2.0.pre.',
-    )
-    this.textScaleFactor,
     this.textScaler,
     this.maxLines,
     this.semanticsLabel,
@@ -38,23 +47,104 @@ class AccessibleRichText extends StatefulWidget {
     this.focusableTextSpanBuilder,
   });
 
+  /// The text to display as a [InlineSpan].
   final TextSpan textSpan;
+
+  /// If non-null, the style to use for this text.
+  ///
+  /// If the style's "inherit" property is true, the style will be merged with
+  /// the closest enclosing [DefaultTextStyle]. Otherwise, the style will
+  /// replace the closest enclosing [DefaultTextStyle].
   final TextStyle? style;
+
+  /// {@macro flutter.painting.textPainter.strutStyle}
   final StrutStyle? strutStyle;
+
+  /// How the text should be aligned horizontally.
   final TextAlign? textAlign;
+
+  /// The directionality of the text.
+  ///
+  /// This decides how [textAlign] values like [TextAlign.start] and
+  /// [TextAlign.end] are interpreted.
+  ///
+  /// This is also used to disambiguate how to render bidirectional text. For
+  /// example, if the [data] is an English phrase followed by a Hebrew phrase,
+  /// in a [TextDirection.ltr] context the English phrase will be on the left
+  /// and the Hebrew phrase to its right, while in a [TextDirection.rtl]
+  /// context, the English phrase will be on the right and the Hebrew phrase on
+  /// its left.
+  ///
+  /// Defaults to the ambient [Directionality], if any.
   final TextDirection? textDirection;
+
+  /// Used to select a font when the same Unicode character can
+  /// be rendered differently, depending on the locale.
+  ///
+  /// It's rarely necessary to set this property. By default its value
+  /// is inherited from the enclosing app with `Localizations.localeOf(context)`.
+  ///
+  /// See [RenderParagraph.locale] for more information.
   final Locale? locale;
+
+  /// Whether the text should break at soft line breaks.
+  ///
+  /// If false, the glyphs in the text will be positioned as if there was unlimited horizontal space.
   final bool? softWrap;
+
+  /// How visual overflow should be handled.
+  ///
+  /// If this is null [TextStyle.overflow] will be used, otherwise the value
+  /// from the nearest [DefaultTextStyle] ancestor will be used.
   final TextOverflow? overflow;
-  final double? textScaleFactor;
+
+  /// An optional maximum number of lines for the text to span, wrapping if necessary.
+  /// If the text exceeds the given number of lines, it will be truncated according
+  /// to [overflow].
+  ///
+  /// If this is 1, text will not wrap. Otherwise, text will be wrapped at the
+  /// edge of the box.
+  ///
+  /// If this is null, but there is an ambient [DefaultTextStyle] that specifies
+  /// an explicit number for its [DefaultTextStyle.maxLines], then the
+  /// [DefaultTextStyle] value will take precedence. You can use a [RichText]
+  /// widget directly to entirely override the [DefaultTextStyle].
   final int? maxLines;
+
+  /// {@macro flutter.widgets.Text.semanticsLabel}
   final String? semanticsLabel;
+
+  /// {@macro flutter.painting.textPainter.textWidthBasis}
   final TextWidthBasis? textWidthBasis;
+
+  /// {@macro dart.ui.textHeightBehavior}
   final ui.TextHeightBehavior? textHeightBehavior;
+
+  /// The color to use when painting the selection.
+  ///
+  /// This is ignored if [SelectionContainer.maybeOf] returns null
+  /// in the [BuildContext] of the [Text] widget.
+  ///
+  /// If null, the ambient [DefaultSelectionStyle] is used (if any); failing
+  /// that, the selection color defaults to [DefaultSelectionStyle.defaultColor]
+  /// (semi-transparent grey).
   final Color? selectionColor;
+
+  /// {@macro flutter.painting.textPainter.textScaler}
   final TextScaler? textScaler;
+
+  /// Fire when a interactive [TextSpan] with [TapGestureRecognizer] is focused
+  /// using keyboard navigation, with its index and [FocusNode] in the [TextSpan]
+  /// tree
   final OnTextSpanFocusChanged? onTextSpanFocused;
+
+  /// [TextStyle] that will be chosen for displayed focused interactive
+  /// [TextSpan], default to [defaultFocusedTextStyle], which will render a
+  /// [Theme.of(context).focusedColor] color
   final FocusedStyleBuilder focusedStyle;
+
+  /// Object that manage the creation and dispose of [FocusNode] of interactive
+  /// [TextSpan]s
   final FocusableTextSpanBuilder? focusableTextSpanBuilder;
 
   @override
@@ -119,7 +209,6 @@ class _AccessibleRichTextState extends State<AccessibleRichText> {
       locale: widget.locale,
       softWrap: widget.softWrap,
       overflow: widget.overflow,
-      textScaleFactor: widget.textScaleFactor,
       maxLines: widget.maxLines,
       semanticsLabel: widget.semanticsLabel,
       textWidthBasis: widget.textWidthBasis,
